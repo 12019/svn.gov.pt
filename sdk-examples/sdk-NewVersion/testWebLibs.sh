b211289878db3ad5170c962e9ce197d1b9b629f3
vsilva <vsilva@7e13cfa0-4879-45a3-b1e0-54bb9da6c43a>
2012-03-09 19:49:32 +0000

    Update SDK Examples

---
+++ sdk-examples/sdk-NewVersion/testWebLibs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+URL_LIBS_TO_TEST=http://projects.caixamagica.pt/projects/cartaocidadao/repository/middleware-offline/builds/2012-03-06/caixamagica-16-32/pteid-pkg-web.tar.gz
+
+
+URL_TEST_SUITE=http://projects.caixamagica.pt/projects/cartaocidadao/repository/middleware-offline/tools/test-suite/sdk-NewVersion/java/src
+WORK_DIR=~/test_web_libs_pteid
+
+mkdir -p $WORK_DIR
+echo  $WORK_DIR
+rm -rf $WORK_DIR/*
+cd $WORK_DIR
+
+## Export and extract Libs to be tested:
+svn export $URL_LIBS_TO_TEST pkg_to_test.tar.gz
+tar xzvf pkg_to_test.tar.gz
+
+
+## Export suite-tests to run tests on SDK:
+svn export $URL_TEST_SUITE java-test
+cd java-test
+
+echo "Now to test your libs:"
+echo "cd $WORK_DIR/java-test"
+echo "make"
+
+
